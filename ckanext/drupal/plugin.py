18c6ce5934f48fda1ccc75883c0f58a09474e473
kindly <kindly@gmail.com>
2011-07-06 17:34:59 +0100

    add package table

--- ckanext/drupal/plugin.py
+++ ckanext/drupal/plugin.py
@@ -1,9 +1,37 @@
 from ckan.plugins import IConfigurer
 from ckan.plugins import implements, SingletonPlugin
+from sqlalchemy import types, Column, Table
+from sqlalchemy import MetaData, create_engine
 
 class Drupal(SingletonPlugin):
     '''initial test of plugin'''
     implements(IConfigurer)
 
+
     def update_config(self, config):
-        config['ckan.site_title'] = 'CKAN-drupal'
+        config['ckan.site_title'] = 'CKAN-Drupal'
+
+        url = config['drupal.db_url'] 
+
+        self.engine = create_engine(url)
+        self.metadata = MetaData()
+
+        PACKAGE_NAME_MAX_LENGTH = 100
+        PACKAGE_VERSION_MAX_LENGTH = 100
+
+        package_table = Table('ckan_package', self.metadata,
+            Column('id', types.UnicodeText, primary_key=True),
+            Column('name', types.Unicode(PACKAGE_NAME_MAX_LENGTH),
+                   nullable=False, unique=True),
+            Column('title', types.UnicodeText),
+            Column('version', types.Unicode(PACKAGE_VERSION_MAX_LENGTH)),
+            Column('url', types.UnicodeText),
+            Column('author', types.UnicodeText),
+            Column('author_email', types.UnicodeText),
+            Column('maintainer', types.UnicodeText),
+            Column('maintainer_email', types.UnicodeText),                      
+            Column('notes', types.UnicodeText),
+            Column('license_id', types.UnicodeText),
+        )
+        self.metadata.create_all(self.engine)
+
