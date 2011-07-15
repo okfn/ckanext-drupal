e23e29f835a993bb58cd5a91bf2eb781af7e2510
kindly <kindly@gmail.com>
2011-07-15 15:14:24 +0100

    let drupal make the nodes

--- ckanext/drupal/plugin.py
+++ ckanext/drupal/plugin.py
@@ -8,81 +8,42 @@ import time
 from ckan.plugins import IConfigurer, ISession
 from ckan.plugins import implements, SingletonPlugin
 import ckan.model as model
+from ckan.logic.action import create, update
+from ckan.controllers.api import ApiController
 
+def drupal_package_create(context, data_dict):
+
+    session = context['model'].Session
+    context['nid'] = data_dict.pop('nid')
+    context['vid'] = data_dict.pop('vid')
+    package_create = create.package_create(context, data_dict)
+    package_create['nid'] = context['nid']
+    package_create['vid'] = context['vid']
+    package_create['revision_message'] = '%s-%s'%(session.revision.id,session.revision.message)
+    return package_create
+
+def drupal_package_update(context, data_dict):
+    session = context['model'].Session
+    context['nid'] = data_dict.pop('nid')
+    context['vid'] = data_dict.pop('vid')
+    package_create = update.package_update(context, data_dict)
+    package_create['nid'] = context['nid']
+    package_create['vid'] = context['vid']
+    package_create['revision_message'] = '%s-%s'%(session.revision.id,session.revision.message)
+    return package_create
 
 class Drupal(SingletonPlugin):
     '''initial test of plugin'''
     implements(IConfigurer)
     implements(ISession, inherit=True)
 
-    def update_node(self, row, session, conn):
-
-        timestamp=int(time.time())
-
-        result = conn.execute(
-            self.node_revisions.insert().values(
-                nid=0,
-                uid=1,
-                title=row.get('title',''),
-                body=row.get('notes',''),
-                teaser=row.get('title',''),
-                log='%s-%s'%(session.revision.id,session.revision.message),
-                timestamp=timestamp,
-                format=1
-            )
-        )
-        vid = result.inserted_primary_key[0]
-        
-
-        if 'nid' in row:
-            result = conn.execute(
-                self.node.update().where(
-                    self.node.c.nid == row['nid']
-                ).values(
-                    vid=vid,
-                    title=row.get('title',''),
-                )
-            )
-            nid = row['nid']
-        else:
-            result = conn.execute(
-                self.node.insert().values(
-                    vid=vid,
-                    type='package',
-                    language='',
-                    title=row.get('title',''),
-                    uid=1,
-                    status=0,
-                    created=timestamp,
-                    changed=timestamp,
-                    comment=2,
-                    promote=0,
-                    moderate=0,
-                    sticky=0,
-                    tnid=0,
-                    transalate=0,
-                )
-            )
-            nid = result.inserted_primary_key[0]
-            row['nid'] = nid
-
-        conn.execute(
-            self.node_revisions.update().where(
-                self.node_revisions.c.vid == vid).values(
-                    nid=nid,
-                )
-        )
-        return nid
-
     def get_package_row(self, conn, package_id):
-
         return conn.execute(
             select(
                 [self.package_table],
                 self.package_table.c.id == package_id
             )
         ).fetchone()
-
             
     def update_drupal(self, session, conn):
 
@@ -90,11 +51,15 @@ class Drupal(SingletonPlugin):
         new = obj_cache['new']
         changed = obj_cache['changed']
         deleted = obj_cache['deleted']
+        nid = session._context['nid']
+        vid = session._context['vid']
 
         try:
-            update_date = session.revision.timestamp
+            update_date = int(
+                time.mktime(session.revision.timestamp.timetuple())
+            )
         except AttributeError:
-            update_date = datetime.datetime.now()
+            update_date = int(time.time())
 
         package_rows = {}
         package_tags = {}
@@ -102,8 +67,6 @@ class Drupal(SingletonPlugin):
         inserts = []
         updates = []
         deletes = []
-
-        updated_nids = []
 
         for obj in new:
             if hasattr(obj, 'state') and 'pending' in obj.state:
@@ -118,8 +81,6 @@ class Drupal(SingletonPlugin):
                 inserts.append(self.add_insert(obj, self.resource_table))
             if isinstance(obj, (model.PackageExtra, model.PackageExtraRevision)):
                 inserts.append(self.add_insert(obj, self.package_extra_table))
-            if isinstance(obj, (model.PackageTag, model.PackageTagRevision)):
-                inserts.append(self.add_tag(obj, conn))
 
         for obj in changed:
             if hasattr(obj, 'state') and 'pending' in obj.state:
@@ -132,8 +93,6 @@ class Drupal(SingletonPlugin):
                     obj.id,
                     self.get_package_row(conn, obj.id)
                 )
-                
-                update['nid'] = package_row['nid']
                 package_rows[update['id']] = update
                 updates.append(update)
             if isinstance(obj, (model.Resource, model.ResourceRevision)):
@@ -146,9 +105,6 @@ class Drupal(SingletonPlugin):
                     deletes.append(self.add_delete(obj, self.package_extra_table, conn))
                 else:
                     updates.append(self.add_update(obj, self.package_extra_table))
-            if isinstance(obj, (model.PackageTag, model.PackageTagRevision)):
-                if obj.state == 'deleted':
-                    deletes.append(self.remove_tag(obj, conn))
 
         for obj in deleted:
             if hasattr(obj, 'state') and 'pending' in obj.state:
@@ -165,8 +121,6 @@ class Drupal(SingletonPlugin):
                 deletes.append(self.add_delete(obj, self.package_extra_table, conn))
             if isinstance(obj, (model.Resource, model.ResourceRevision)):
                 deletes.append(self.add_delete(obj, self.resource_table, conn))
-            if isinstance(obj, (model.PackageTag, model.PackageTagRevision)):
-                deletes.append(self.remove_tag(obj, conn))
 
         for row in inserts + updates + deletes:
             if 'package_id' in row:
@@ -179,40 +133,23 @@ class Drupal(SingletonPlugin):
                 package_row = self.get_package_row(conn, package_id)
                 update = {'__table': self.package_table, 
                           'id': package_id,
-                          'nid': package_row['nid'],
-                          'update_date': update_date,
-                          'title': package_row['title'],
-                          'notes': package_row['notes']}
+                          'update_date': update_date}
                 updates.append(update)
                 package_rows[package_id] = update
 
-        for package_row in package_rows.values():
-            nid = self.update_node(package_row, session, conn)
-            updated_nids.append(nid)
-
-        for row in inserts + updates + deletes:
-            if 'package_id' in row:
-                row['nid'] = package_rows[row['package_id']]['nid']
-
         for row in inserts:
            table = row.pop('__table') 
+           row.update({'nid':nid, 'vid':vid})
            conn.execute(table.insert().values(**row))
 
         for row in updates:
            table = row.pop('__table') 
            id = row.pop('id')
+           row.update({'nid':nid, 'vid':vid})
            conn.execute(table.update().where(table.c.id==id).values(**row))
 
         for row in deletes:
            table = row.pop('__table') 
-           if table == self.term_node:
-               conn.execute(
-                   table.delete().where(
-                       and_(table.c.nid==row['nid'],table.c.tid==row['tid'])
-                   )
-               )
-               continue
-           id = row.pop('id')
            conn.execute(table.delete().where(table.c.id==id))
 
 
@@ -263,63 +200,12 @@ class Drupal(SingletonPlugin):
             delete["package_id"] = package_id
         return delete
 
-    def add_tag(self, obj, conn):
-
-        term = conn.execute(
-            select([self.term_data]).where(
-                and_(
-                    self.term_data.c.vid == self.vid,
-                    self.term_data.c.name == obj.tag.name
-                )
-            )
-        ).fetchone()
-
-        if not term:
-            insert_term = conn.execute(
-                self.term_data.insert().values(
-                    vid=self.vid,
-                    name=obj.tag.name
-                )
-            )
-            tid = insert_term.inserted_primary_key[0]
-        else:
-            tid = term['tid']
-
-        return {
-            '__table': self.term_node,
-            'package_id': obj.package_id,
-            'tid': tid,
-            'vid': self.vid
-        }
-
-    def remove_tag(self, obj, conn):
-
-        tid = conn.execute(
-            select([self.term_data]).where(
-                and_(
-                    self.term_data.c.vid == self.vid,
-                    self.term_data.c.name == obj.tag.name
-                )
-            )
-        ).fetchone()['tid']
-
-        return {
-            '__table': self.term_node,
-            'package_id': obj.package_id,
-            'tid': tid,
-            'vid': self.vid
-        }
-
-
     def before_commit(self, session):
-
         session.flush()
         if not hasattr(session, '_object_cache'):
             return
-
         conn = self.engine.connect()
         trans = conn.begin()
-
         try:
             self.update_drupal(session, conn)
             trans.commit()
@@ -334,7 +220,6 @@ class Drupal(SingletonPlugin):
         config['ckan.site_title'] = 'CKAN-Drupal'
 
         url = config['drupal.db_url'] 
-        self.vid = int(config.get('drupal.vid', 1))
 
         self.engine = create_engine(url, echo=True)
         self.metadata = MetaData(self.engine)
@@ -342,8 +227,14 @@ class Drupal(SingletonPlugin):
         PACKAGE_NAME_MAX_LENGTH = 100
         PACKAGE_VERSION_MAX_LENGTH = 100
 
+        ApiController.register_action('drupal_package_create',
+                                       drupal_package_create)
+        ApiController.register_action('drupal_package_update',
+                                       drupal_package_update)
+
         self.package_table = Table('ckan_package', self.metadata,
             Column('nid', types.Integer, unique=True),
+            Column('vid', types.Integer, unique=True),
             Column('id', types.Unicode(100), primary_key=True),
             Column('name', types.Unicode(PACKAGE_NAME_MAX_LENGTH),
                    nullable=False, unique=True),
@@ -356,7 +247,7 @@ class Drupal(SingletonPlugin):
             Column('maintainer_email', types.UnicodeText),                      
             Column('notes', types.UnicodeText),
             Column('license_id', types.UnicodeText),
-            Column('update_date', types.DateTime),
+            Column('update_date', types.Integer),
             Column('state', types.UnicodeText),
             Column('completed', types.Boolean),
         )
@@ -364,6 +255,7 @@ class Drupal(SingletonPlugin):
         self.resource_table = Table(
             'ckan_resource', self.metadata,
             Column('nid', types.Integer),
+            Column('vid', types.Integer),
             ## cache of package id to make things easier
             Column('package_id', types.UnicodeText),
             ##
@@ -380,20 +272,12 @@ class Drupal(SingletonPlugin):
 
         self.package_extra_table = Table('ckan_package_extra', self.metadata,
             Column('nid', types.Integer),
+            Column('vid', types.Integer),
             Column('id', types.Unicode(100), primary_key=True),
             Column('package_id', types.UnicodeText),
             Column('key', types.UnicodeText),
             Column('value', types.UnicodeText),
         )
-
-        self.node = Table('node', self.metadata,
-                          autoload = True)
-        self.node_revisions = Table('node_revisions', self.metadata,
-                                   autoload = True)
-        self.term_node = Table('term_node', self.metadata,
-                                   autoload = True)
-        self.term_data = Table('term_data', self.metadata,
-                                   autoload = True)
 
         self.metadata.create_all(self.engine)
 
